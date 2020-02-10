edbaa5a2625377386c93c3605565ef903464fff3
Silver3310 <epifanov998@mail.ru>
2020-02-10 15:29:31 +0700

    Add files for PyPi

--- README.md
+++ README.md
@@ -83,4 +83,4 @@ python manage.py migrate
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+This project is licensed under the MIT License - see the [LICENSE.txt](LICENSE.txt) file for details
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name='django-wallets',
+  packages=['django-wallets'],
+  version='0.1',
+  license='MIT',
+  description='Apple Wallet integration for a django a project',
+  author='Elivanov Alexey',
+  author_email='epifanov998@mail.ru',
+  url='https://github.com/Silver3310/django-wallets',
+  download_url='https://github.com/Silver3310/django-wallets/archive/v_01.tar.gz',
+  keywords=['django', 'wallet', 'apple', 'pass'],
+  install_requires=[
+          'celery',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
